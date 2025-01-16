2ec810a7a48c1686be2021056c36d1e0a83a8191
tigocomtigo <thiagosalustiano@alunos.utfpr.edu.br>
2025-01-16 09:16:19 -0300

    feat: combate concluído

---
+++ 2dzin/characters/knight/knight.gd
@@ -0,0 +1,99 @@
+extends CharacterBody3D
+
+@onready var animation_tree = $"AnimationTree"
+@onready var anim_play = $"Knight/AnimationPlayer"
+@onready var life_bar = $InitialHUD/ProgressBar
+@onready var hbox = $InitialHUD/HBoxContainer
+var damage = 400
+var life = 600
+var defesa: bool = false
+var counter: bool = false
+var pontos_defesa = 10
+var turn = true
+
+func _ready() -> void:
+	randomize()
+
+func _physics_process(delta: float) -> void:
+	var state_machine = animation_tree.get("parameters/playback") as AnimationNodeStateMachinePlayback
+	var current_animation = state_machine.get_current_node()
+	life_bar.set_value_no_signal(life)
+	if life <= 0:
+		animation_tree.set("parameters/conditions/died", true)
+	elif(current_animation != "Idle"):
+		animation_handler(current_animation)
+	
+	pass
+
+func receive_attack(damage):
+	if defesa:
+		defence_handler(damage)
+		defesa = false
+	else:
+		animation_tree.set("parameters/conditions/hit", true)
+		life -= damage
+
+func _on_atacar_pressed() -> void:
+	hbox.visible = false
+	if turn:
+		animation_tree.set("parameters/conditions/attack", true)
+		turn = false
+	pass # Replace with function body.
+
+func _on_attack_trigger() -> void:
+	get_tree().call_group("enemies","receive_attack", damage)
+	pass
+
+func _on_counter_attack_trigger() -> void:
+	get_tree().call_group("enemies","receive_attack", damage)
+	animation_tree.set("parameters/conditions/attack", false)
+
+func _on_fugir_pressed() -> void:
+	
+	pass # Replace with function body.
+
+func animation_handler(current_animation):
+	if(current_animation == "Block"):
+		animation_tree.set("parameters/conditions/defend", false)
+	if(current_animation == "Attack"):
+		animation_tree.set("parameters/conditions/attack", false)
+	animation_tree.set("parameters/conditions/hit", false)
+	pass
+
+
+func _on_defender_pressed() -> void:
+	hbox.visible = false
+	if turn:
+		defesa = true
+		turn = false
+		get_tree().call_group("enemies","change_turn")
+	pass # Replace with function body.
+
+func defence_handler(damage):
+	var random = randi() % pontos_defesa
+	if(random < 3):
+		animation_tree.set("parameters/conditions/hit", true)
+		life -= damage
+	elif(random < 6):
+		animation_tree.set("parameters/conditions/defend", true)
+		life -= damage/2
+	else:
+		animation_tree.set("parameters/conditions/defend", true)
+		animation_tree.set("parameters/conditions/attack", true)
+		counter = true
+	pass
+
+func change_turn():
+	turn = true
+
+func pass_turn():
+	get_tree().call_group("enemies","change_turn")
+	pass
+
+func show_combat_options():
+	hbox.visible = true
+
+func animation_counter_handler():
+	if !counter:
+		hbox.visible = true
+		counter = false
---
+++ 2dzin/characters/skeleton/skeleton.gd
@@ -0,0 +1,53 @@
+extends CharacterBody3D
+
+@onready var animation_tree = $AnimationTree
+var life = 1200
+var turn: bool = false; # Define se é o turno do inimigo
+const base_damage = 200
+
+func _ready() -> void:
+	randomize()
+	pass
+
+func _physics_process(delta: float) -> void:
+	var state_machine = animation_tree.get("parameters/playback") as AnimationNodeStateMachinePlayback
+	var current_animation = state_machine.get_current_node()
+	
+	if life <= 0:
+		animation_tree.set("parameters/conditions/died", true)
+	if(current_animation != "Idle"):
+		animation_handler()
+	elif (turn == true && current_animation == "Idle"):
+		attack()
+		turn = false
+	pass
+
+func attack():
+	
+	animation_tree.set("parameters/conditions/attack", true)
+	pass
+
+func attack_trigger() -> void:
+	get_tree().call_group("player","receive_attack", base_damage)
+	pass
+
+func receive_attack(damage):
+	var random = randi() % 20
+	if(random <= 14):
+		animation_tree.set("parameters/conditions/attacked", true)
+		life -= damage
+	else:
+		animation_tree.set("parameters/conditions/dodge", true)
+	pass
+
+func animation_handler():
+	animation_tree.set("parameters/conditions/attacked", false)
+	animation_tree.set("parameters/conditions/attack", false)
+	animation_tree.set("parameters/conditions/dodge", false)
+	pass
+
+func change_turn():
+	turn = true
+
+func pass_turn():
+	get_tree().call_group("player","change_turn")
